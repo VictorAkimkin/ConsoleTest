d20828359ec87c109bd53dc539d113055c1f6435
Victor Akimkin <akimkinvictor@gmail.com>
2022-10-15 20:13:14 +0300

    15.10.2022

--- Converting.h
+++ Converting.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <iostream>
-void iFC(float&& f) { std::cout << "float= " << f << std::endl; }
+void iFC(float && f) { std::cout << "float= " << f << std::endl; }
 void iFC(int&& i) { std::cout << "int= " << i << std::endl; }
+
+//вызовутся для(iFC(x), ...) если разкоментировать
+//void iFC(float& f) { std::cout << "float&= " << f << std::endl; } 
+//void iFC(int& i) { std::cout << "int&= " << i << std::endl; }
 
 template <typename... T>
 void convert(T &&... x)
 {
 	(iFC(x), ...); // конвертирует float в int&& и вызывает f(int&&), конвертирует int в float&& и вызывает f(float&&)
 	(iFC(std::forward<T>(x)), ...);// передает float как float&&, а int как int&& с соответсвующими вызами f
+	(iFC(std::move(x)), ...);// передает float как float&&, а int как int&& с соответсвующими вызами f
 }
